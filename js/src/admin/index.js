aa1f2a672b8d91b37a4ff74f8f70737fd2832d6c
Ian Morland <ian@morland.me>
2020-12-06 22:56:23 +0000

    Ready up for beta 15

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,13 +1,11 @@
-import { extend } from 'flarum/extend';
 import app from 'flarum/app';
-import PermissionGrid from 'flarum/components/PermissionGrid';
 
-app.initializers.add('flarumite-admin-simple-discussion-views', () => {
-    extend(PermissionGrid.prototype, 'moderateItems', (items) => {
-        items.add('resetViews', {
-            icon: 'far fa-eye',
-            label: app.translator.trans('flarumite-simple-discussion-views.admin.permissions.reset_views_label'),
-            permission: 'discussion.resetViews',
-        });
-    });
+app.initializers.add('flarumite-simple-discussion-views', () => {
+  app.extensionData
+    .for('flarumite-simple-discussion-views')
+    .registerPermission({
+      icon: 'far fa-eye',
+      label: app.translator.trans('flarumite-simple-discussion-views.admin.permissions.reset_views_label'),
+      permission: 'discussion.resetViews'
+    }, 'moderate');
 });
