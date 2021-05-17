34a7fe6047c07592e10d8ff47ffbeeae5e10310c
Ian Morland <ian@morland.me>
2021-05-17 13:27:18 +0100

    Simplify migration

--- migrations/2020_04_27_000000_add_view_count_column.php
+++ migrations/2020_04_27_000000_add_view_count_column.php
@@ -1,18 +1,13 @@
 
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Schema\Builder;
+use Flarum\Database\Migration;
 
-return [
-    'up' => function (Builder $schema) {
-        if (!$schema->hasColumn('discussions', 'view_count')) {
-            $schema->table('discussions', function (Blueprint $table) {
-                $table->integer('view_count')->default(0)->unsigned();
-            });
-        }
-    },
-    'down' => function (Builder $schema) {
-        // no.
-    },
-];
+return Migration::addColumns('discussions', [
+    'view_count' => [
+        'integer',
+        'null' => false,
+        'default' => 0,
+        'unsigned' => true,
+    ],
+]);
