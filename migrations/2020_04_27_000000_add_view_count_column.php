9f05dde75e1020a9d1006747e2464f9047c234c1
Ian Morland <ian@morland.me>
2021-06-09 20:05:42 +0100

    Migration fix (#17)
    * Remove newline at head of file
    
    * Apply fixes from StyleCI
    
    * Update Flarum webpack
    
    Co-authored-by: Ian Morland <imorland@users.noreply.github.com>

--- migrations/2020_04_27_000000_add_view_count_column.php
+++ migrations/2020_04_27_000000_add_view_count_column.php
@@ -1,5 +1,13 @@
-
 <?php
+
+/*
+ * This file is part of flarumite/simple-discussion-views.
+ *
+ * Copyright (c) 2020 Flarumite.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 use Flarum\Database\Migration;
 
