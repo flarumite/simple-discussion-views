bc914bbe7261a983017c393f787eccae6d03c549
Ian Morland <ian@morland.me>
2022-01-26 11:42:55 +0000

    Address misplaced view count in discussion list

--- js/src/forum/components/AddViewsToModelAndDisplay.js
+++ js/src/forum/components/AddViewsToModelAndDisplay.js
@@ -3,12 +3,13 @@ import Model from 'flarum/common/Model';
 import Discussion from 'flarum/common/models/Discussion';
 import DiscussionListItem from 'flarum/common/components/DiscussionListItem';
 import abbreviateNumber from 'flarum/common/utils/abbreviateNumber';
+import icon from 'flarum/common/helpers/icon';
 
 export default function () {
   Discussion.prototype.views = Model.attribute('views');
 
   extend(DiscussionListItem.prototype, 'infoItems', function (items) {
     const views = this.attrs.discussion.views();
-    items.add('discussion-views', <span>{abbreviateNumber(views)}</span>);
+    items.add('discussion-views', <span>{icon('far fa-eye')} {abbreviateNumber(views)}</span>);
   });
 }
