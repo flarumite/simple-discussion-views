0b8077ac107415b560d418d2e354393c27549c6f
IanM <16573496+imorland@users.noreply.github.com>
2023-06-20 18:40:30 +0100

    chore: use fe extenders (#34)
    * chore: use fe extenders
    
    * chore: format

---
+++ js/src/forum/extend.ts
@@ -0,0 +1,4 @@
+import Extend from 'flarum/common/extenders';
+import Discussion from 'flarum/common/models/Discussion';
+
+export default [new Extend.Model(Discussion).attribute<boolean>('canReset').attribute<number>('views')];
--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -2,12 +2,10 @@ import app from 'flarum/forum/app';
 import AddPopularSort from './components/AddPopularSort';
 import AddViewsToModelAndDisplay from './components/AddViewsToModelAndDisplay';
 import AddModerationControl from './components/AddModerationControl';
-import Discussion from 'flarum/common/models/Discussion';
-import Model from 'flarum/common/Model';
+
+export { default as extend } from './extend';
 
 app.initializers.add('flarumite-simple-discussion-views', () => {
-  Discussion.prototype.canReset = Model.attribute('canReset');
-
   AddPopularSort();
   AddViewsToModelAndDisplay();
   AddModerationControl();
