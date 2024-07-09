c4432a12b5de0002391f8cf269158d816e3cee6c
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2024-07-09 10:11:39 +0200

    style: run prettier

--- js/src/@types/shims.d.ts
+++ js/src/@types/shims.d.ts
@@ -1,6 +1,6 @@
 declare module 'flarum/common/models/Discussion' {
-	export default interface Discussion {
-		canReset(): boolean;
-        views(): number;
-	}
+  export default interface Discussion {
+    canReset(): boolean;
+    views(): number;
+  }
 }
